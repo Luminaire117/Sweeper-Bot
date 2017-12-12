f554e3faa132da86b4ca76a1f4a799f1d31a9412
Mike Wohlrab <Mike@Layer7.Solutions>
2017-12-12 06:25:40 -0600

    Updated maintenance times, hard coding muted role so see if that speeds up the mute process.

--- src/util/lib/listeners/Events.ts
+++ src/util/lib/listeners/Events.ts
@@ -456,9 +456,9 @@ export class Events {
 										`\n\n` +
 										`Please note the following planned Destiny maintenance event(s).` +
 										`\n\n` +
-										`__**Tuesday, December 5, 2017 [2017-12-05]**__\n` +
+										`__**Tuesday, December 12, 2017 [2017-12-12]**__\n` +
 										`**STARTS:** 7 AM PST (1500 UTC) \n` +
-										`**ENDS:** 10 AM PST (1800 UTC) \n` +
+										`**ENDS:** 2 PM PST (2200 UTC) \n` +
 										`\n\n` +
 										`Note: Maintenance times, including end time, are subject to change by Bungie without notice. For more info see @BungieHelp on Twitter or <https://www.bungie.net/en/Help/Article/13125> Thank you.`);
 					return;
